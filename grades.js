4478cabc337c9bd4813c1d39c2cec24cd9b48908
C Wilkosz <theewilkyway@gmail.com>
2017-01-28 09:11:07 -0600

    satbud

---
+++ grades.js
@@ -0,0 +1,49 @@
+//How many of each grade?
+//What is the lowest grade?
+//What is the highest grade?
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+//if (x > 5) {
+ /* do the right thing */
+//} else if (x > 50) {
+ /* do the right thing */
+//}/// else {
+ /* do the right thing */
+//}
+
+// var of each
+var mixedCrap = ["A","B","C","D","E","F"];
+console.log(" A B C D E F ", mixedCrap.sort())
+		
+
+//var mixedCrap = ["r", "m", 33, 9];
+//console.log("????",mixedCrap.sort());
+//var show lowest
+
+//show highest
+
+if (avg < 60){
+	console.log(" : F ");
+}
+else if (avg < 70){
+	console.log(" : D ");
+}
+else if (avg < 80){
+	console.log(" : C ");
+}
+else if (avg < 90){
+	console.log(" : B ");
+}
+else if (avg < 100){
+	console.log(" : A ");	
+}
+
+console.log("the grades");
+console.log(" A ", theGrades[0]);
+console.log(" B ", theGrades[0]);
+console.log(" C ", theGrades[0]);
+console.log(" D ", theGrades[0]);
+console.log(" f ", theGrades[0]); 
+
+
+
